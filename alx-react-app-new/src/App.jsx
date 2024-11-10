f57e8d57c834f7e93122b07549dda7a7da62047b
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-11-11 00:08:25 +0200

    Adding a counter feature

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <div>
+        <Counter />
       <WelcomeMessage />
           <Header />
           <MainContent />
