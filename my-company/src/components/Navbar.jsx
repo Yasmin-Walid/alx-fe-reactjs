c3769d8124c3267664bc6243dfd9ad302a06ff0f
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-11-14 23:37:14 +0200

    adding styles and adjusting the navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
+const Navbar = () => (
+
+    <nav 
+    style={{ display: 'flex', justifyContent: 'space-around', padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+       <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+       <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+       <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+       <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+ );
+ export default Navbar;
