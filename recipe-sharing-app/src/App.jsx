69f28199b6a635ce46a4348c50be0110a12bf936
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-11-17 21:47:19 +0200

    adding more functions

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -10,6 +10,17 @@ import SearchBar from './components/SearchBar';
         <h1>Recipe Sharing Application</h1>
         <SearchBar />
         <RecipeList />
+        <AddRecipeForm />
+        <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
       </div>
     );
   };
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,19 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
   searchTerm: '',
   setSearchTerm: (term) => set({ searchTerm: term }),
   filteredRecipes: [],
