fe47f88b3eada339daa9f111ba0991f846e39cb2
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-11-24 03:08:43 +0200

    /blog/:id in App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
     return (
-        <Router>
+        <Router> {/* Ensure BrowserRouter is wrapping the Routes */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route
@@ -13,8 +18,12 @@ const App = () => {
                         </ProtectedRoute>
                     }
                 />
-                <Route path="/blog/:postId" element={<BlogPost />} />
+                {/* Dynamic Route for Blog Post */}
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="*" element={<Navigate to="/" replace />} /> {/* Catch-all for unknown routes */}
             </Routes>
         </Router>
     );
 };
+
+export default App;
