6a12ed5f1900ec60cf8ae3800d023e1eb9474c8d
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-12-01 21:47:42 +0200

    fixing syntax

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@
 function UserProfile() {
     return (
       <div className=' user-profile 
-      bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xs max-w-sm '
+      bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm '
       >
       <img src='/image.jpg' alt='working man' className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md: w-36 h-36"/>
         <h1 className="text-xl text-blue-800 my-4 sm:text-lg
