8a64bd763b3130111dc4d2b300945eac4d6dccbc
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-12-08 20:20:46 +0200

    adding md to styling

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -29,13 +29,13 @@ const RecipeComponent = () => {
   return (
     <div className="container mx-auto p-4">
     <h1 className="text-3xl font-bold mb-6 text-center text-green-800">Recipes</h1>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:grid-cols-2 lg:grid-cols-3">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow">
+        <div key={recipe.id} className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow max-w-md ">
           <img
             src={recipe.image}
             alt={recipe.title}
-            className="w-full h-40 object-cover rounded-t-lg"
+            className="w-full h-40 object-cover "
           />
           <h2 className="text-xl font-semibold mt-4">{recipe.title}</h2>
           <p className="text-green-800 mt-2 font-semibold">{recipe.summary}</p>
