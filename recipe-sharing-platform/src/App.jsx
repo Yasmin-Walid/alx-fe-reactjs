6db641eb1db8e758dc7cb730f88f22f143959466
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-12-08 21:16:27 +0200

    React Router set up

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 import HomePage from './components/HomePage';
 import React from "react";
 import './index.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      < HomePage />
-    </>
+    <Router>
+      <Routes>
+      <Route  path = "/" element = {<HomePage />} />
+      <Route path ="/recipe.id" element={< RecipeDetail />} />
+      </Routes>
+      </Router>
   )
 }
 
