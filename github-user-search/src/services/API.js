1679221689b65f4236474857f5a7d858c057db63
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-12-21 15:12:56 +0200

    delet .env

--- github-user-search/src/services/API.js
+++ github-user-search/src/services/API.js
@@ -1,2 +1,9 @@
 const GITHUB_API_KEY = process.env.REACT_APP_GITHUB_API_KEY;
 const GITHUB_API_URL = "https://api.github.com";
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_GITHUB_API_URL;
+
+export const fetchUser = (username) => {
+    return axios.get(`${API_URL}/users/${username}`);
+};
